5e862a128e218b991ee8ff054963896b2850365f
RaghaviP <raghav88pp@gmail.com>
2021-11-28 11:59:40 +0530

    worked with is statememnt in pycharm

---
+++ If_Elif_Else_Statement_in_Python.py
@@ -0,0 +1,33 @@
+x = 7
+r x % 2
+
+if r ==0:
+      print("even")
+
+      if(r==1):
+          print("odd")
+else:
+           if r=x>5
+print("bye")
+
+x = 2
+
+elif x ==1:
+    print("one)"
+
+elif (x ==2):
+      print("two")
+
+what if the value 5
+
+
+
+
+
+
+
+
+
+
+
+
