73d99d83cdf49a75fa7b89685d124692e4c3e78c
RaghaviP <raghav88pp@gmail.com>
2021-11-29 23:02:49 +0530

    coding done with If

---
+++ If.py
@@ -0,0 +1,44 @@
+# python program to illustrates
+'''
+i = 10
+if (i > 15):
+    print("10 is less than 15")
+print("I am Not in if")
+'''
+
+# python program illustrate 1.
+'''
+A = 5 A is nothing but variable 5
+if (A < 20): 
+    print("5 is less than 20")
+print("How are you")
+'''
+'''
+Age = int(input("Enter your age"))
+if (Age >= 18):
+    print("you are eligible to vote")
+else:
+    print("Sorry! you have to wait!!")
+'''
+
+A = 5
+'''
+if A == 1:
+    print ("one")
+
+elif(A == 2):
+    print ("two")
+elif(A == 3):
+    print("three")
+elif(A == 4):
+    print("four")
+else:
+    print("wrong Input")
+'''
+'''
+A = 10
+if(A > 20):
+    print("10 is less than 20")
+print("I am not Good")
+'''
+
