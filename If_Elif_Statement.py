524c8fea7b05f3006ad34420b472b6c2539a1099
RaghaviP <raghav88pp@gmail.com>
2021-11-30 22:04:29 +0530

    Illustrate coding

---
+++ If_Elif_Statement.py
@@ -0,0 +1,55 @@
+'''
+age = int(input("Enter your age"))
+if (age >= 15):
+    print ("you are eligible to vote")
+else:
+    print("sorry! you have to wait")
+'''
+'''
+X = 6
+
+if X == 1:
+    print ("one")
+
+elif(X == 2):
+    print ("two")
+elif (X == 3):
+    print("three")
+elif (X == 4):
+    print("four")
+elif (X == 5):
+    print("five")
+else:
+    print("wrong Input")
+'''
+
+'''
+abc = 9
+r = abc % 2
+if r == 0:
+    print("even")
+    if abc > 5:
+        print("great")
+    else:
+        print("not so great")
+else:
+    print("odd")
+'''
+'''
+E = 50
+if(E > 60):
+    print("50 is less than 60")
+print("I am very good Girl")
+'''
+'''
+D = 90
+if (D > 100):
+    print("90 is less than 100")
+print(" I am doing good")
+'''
+'''
+R = 100
+if (R < 150):
+    print("100 is less than 150")
+print("what are you doing")
+'''
