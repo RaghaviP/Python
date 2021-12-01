db923cd8bcc83d933080ceb7fa09136e3fd6b201
RaghaviP <raghav88pp@gmail.com>
2021-12-01 23:15:26 +0530

    coding illustrate

---
+++ If_Else_condition.py
@@ -0,0 +1,44 @@
+'''
+x = 10
+y = 20
+if x > y:
+    print("x is smaller")
+elif x < y:
+    print("y is greater")
+else:
+    print("x and y are equal")
+'''
+'''
+temperature = 40
+if temperature > 60:
+    print("Alarm on")
+else:
+    print("Alarm off")
+'''
+'''
+time = 12
+if time < 12:
+    print("its not exactly")
+else:
+    print("its exactly")
+
+
+age = input("Enter your name")
+if (age <= 100):
+    print("year of month")
+else:
+    print("show birth year")
+'''
+'''
+names = ["Akash", 4, "Raghavi"]
+print(names[1])
+'''
+'''
+X = 999
+if (X < 2000):
+    print("999 is less than 2000")
+elif ("900 is greater than 2000"):
+    print("its invalid")
+
+    print ("its validity")
+'''
