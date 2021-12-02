718c5626e7a530575fd12642b134a92e254b9e56
RaghaviP <raghav88pp@gmail.com>
2021-12-02 20:13:37 +0530

    worked on if

---
+++ Variables_list_if.py
@@ -0,0 +1,33 @@
+'''
+print("2 + 5")
+
+name = 'youtube'
+
+
+square = [ 45,25,85,25]
+square
+
+x = 20
+y = 40
+if x < y:
+    print("its descending order")
+elif x > y:
+    print("its ascending order")
+else:
+    print("both are equal to")
+
+
+size = 37
+if size > 37:
+    print("its not match")
+else:
+    print("its fit")
+
+size = 28
+if size > 28:
+    print("its l")
+elif size < 28:
+    print("its small")
+else:
+    print("does not match")
+'''
