4a2de1ec8c03ccfe6585062f38331879ceeaf136
RaghaviP <raghav88pp@gmail.com>
2021-12-06 19:46:02 +0530

    worked on while loop

---
+++ While_Loop.py
@@ -0,0 +1,96 @@
+'''
+i = 1
+
+while i <=5:
+    print("Raghavi")
+    i = i+1
+
+A = 5
+while A>=1:
+     print("Raghavi")
+     A = A-1
+
+x = 10
+while x>=7:
+    print("python")
+    x = x * 10
+
+abc = 4
+while abc<=1:
+    print("pycharm")
+    abc =abc-1
+
+i = 1
+
+while i<=5:
+    print("Raghavi ",end="")
+    j=1
+    while j<=4:
+        print("poojar ",end="")
+        j=j+1
+
+    i =i+1
+    print()
+
+row = 1
+while row<=5:
+    col=1
+    while col<= row:
+        print('*' ,end='')
+        col+1
+
+    row + 1
+    print()
+
+
+varl = 4
+while(varl > 0):
+    print('varl = '+str(varl))
+    varl = varl-3
+
+    if varl == 2:
+        break
+
+print ("END Program")
+
+
+count = 20
+
+while count > 0:
+    if count % 2 == 0:
+        count -= 1
+        continue
+    print(f'processing Number {count}')
+    count -= 1
+
+
+a = 10
+i = 5
+while i <= a:
+    a -= 1
+    if a == 7:
+        print(" a was equal to 7 once")
+        continue
+    print(a)
+
+
+
+senio = 5
+while senio <= 6:
+
+    print("shri ",end="")
+    bunny = 1
+    while bunny <= 1:
+        print("Ram ",end="")
+        bunny = bunny + 1
+
+
+    senio = senio + 1
+    print()
+
+
+u = 10
+while u <= 20:
+    print("Data Analyst")
+    u = u + 1
+'''
