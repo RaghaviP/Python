c19f8ba29da8ccb2234d07848f06dd390135975b
RaghaviP <raghav88pp@gmail.com>
2021-12-07 19:43:53 +0530

    worked on while

---
+++ 1_while_loop.py
@@ -0,0 +1,83 @@
+
+'''
+a = 1
+while a <= 5:
+    print( a * "Hello Everybody")
+    a = a + 1
+
+
+c = 5
+while c >= 2:
+    print(c * " Hi ")
+    c = c - 1
+
+
+
+w = 20
+while w <= 30:
+    print(w * " see ya ")
+     w = w + 1
+
+
+u = 3
+while u >= 0:
+    print(" while loop ")
+    u = u - 1
+
+
+s = 1
+while s <= 5:
+    print("mamox ", end="")
+    t = 1
+    while t <= 4:
+        print("gaming ",end="")
+        t = t + 1
+
+    s = s + 1
+    print()
+
+
+g = 1
+while g <= 5:
+    print("senio ",end="")
+    h = 1
+    while h <= 4:
+        print("vlogs ",end="")
+        h = h + 1
+
+    g = g + 1
+    print()
+
+
+y = 3
+
+while y <= 6:
+    print("book ",end="")
+    z = 3
+    while z <= 5:
+        print("pen ",end="")
+        z = z + 1
+
+    y = y + 1
+    print()
+
+
+a = 1
+while a <= 5:
+    print("shri ",end="")
+    b = 1
+    while b <= 5:
+        print("Ram ",end="")
+        c = 1
+        while c <= 5:
+            print("Sundar ",end="")
+            c = c + 1
+
+        a = a + 1
+        b = b + 1
+        print()
+    '''
+
+
+
+
