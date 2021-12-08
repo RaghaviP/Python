2782eb2373ba3d0d3a821187f79c2adfab3d4056
RaghaviP <raghav88pp@gmail.com>
2021-12-08 19:12:29 +0530

    Created for loop

---
+++ For_Loop.py
@@ -0,0 +1,61 @@
+
+'''
+a = "RAGHAVI"
+
+for i in a:
+    print(i)
+
+
+for i in [2,6,'paul']:
+    print(i)
+
+
+for i in range(10):
+    print(i)
+
+
+for i in range(11,21,1):
+    print(i)
+
+
+
+for i in range (0,20,5):
+    print(i)
+
+
+for i in range(20,10,-1):
+    print(i)
+
+
+for i in range (1,21):
+    if i%5!=0:
+        print(i)
+
+
+for i in ("Python"):
+    print(i)
+
+
+for i in range (21,46,3):
+    print(i)
+
+
+for i in range (100,0,-5):
+    print(i)
+
+
+for i in range (20,80):
+    if i%4!=0:
+        print(i)
+
+
+
+for i in range (20,1,-1):
+    print(i)
+
+
+for i in range (11,21):
+    if i%15!=0:
+        print(i)
+'''
+
