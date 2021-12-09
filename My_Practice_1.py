c4e3b18aebce2e149b749d8ea0e485564576c0dc
RaghaviP <raghav88pp@gmail.com>
2021-12-09 21:25:13 +0530

    practiced on conditions

---
+++ My_Practice_1.py
@@ -0,0 +1,194 @@
+
+'''
+a = int(input("Enter 1st number"))
+b = int(input("Enter 2nd number"))
+c = a + b
+print(c)
+
+
+D = int(input("Enter 3rd number"))
+E = int(input("Enter 4th number"))
+f = D + E
+print(f)
+
+
+ch = input("enter a char")
+print(ch[1])
+
+
+result = eval(input("enter an expr"))
+print(result)
+
+
+h = int(input("Enter 6th number"))
+i = int(input("Enter 7th  number"))
+j = h + i
+print (j)
+
+
+h = int(input("Enter 6th number"))
+i = int(input("Enter 7th  number"))
+j = h - i
+print(j)
+
+
+h = int(input("Enter 6th number"))
+i = int(input("Enter 7th  number"))
+j = h * i
+print(j)
+
+
+h = int(input("Enter 6th number"))
+i = int(input("Enter 7th  number"))
+j = h / i
+print(j)
+
+
+ch = input("Enter a char")
+print(ch[5])
+
+
+
+ch = input("Enter a char")
+print(ch[8])
+
+
+Python = int(input("Enter an Integer:"))
+print(Python)
+
+
+value = input("Please enter a string:\n")
+print(f" you entered {value}")
+
+
+value = input("Please enter a string:\n")
+print(f" you entered {value}")
+
+
+Akash = input("Enter your Name")
+print(Akash)
+
+
+Age = 45
+
+if Age >= 18:
+    print("You are an Adult")
+    print("you can Vote")
+
+print("Thank you")
+
+
+
+Age = 14
+
+if Age >= 18:
+    print("You are an Adult")
+    print("you can Vote")
+
+print("Thank you")
+
+
+
+Age = 14
+
+if Age <= 18:
+    print("You are not an Adult")
+    print("you cannot Vote")
+
+print("Thank you")
+
+
+x = 8
+r = x % 2
+
+if r == 0:
+    print("Even")
+
+print("Bye")
+
+
+x = 6
+r = x % 2
+
+if r == 0:
+    print("Even")
+    if x > 5:
+        print("Great")
+    else:
+        print("No greater")
+
+else:
+    print("odd")
+
+print("Bye")
+
+
+Age = 25
+
+if Age >= 18:
+    print("You are an Adult")
+    print("you can Vote")
+elif Age < 18 and Age > 3:
+    print("you are in school")
+else:
+    print("you are a child")
+
+
+print("Thank you")
+
+
+r = 1
+
+while r <= 3:
+    print("Raghavi Poojar")
+    r = r + 1
+
+r = 10
+
+while r <= 20:
+    print("Raghavi Poojar")
+    r = r + 1
+
+
+
+a = 2
+while a <= 3:
+    print("Book " ,end="")
+    b = 2
+    while b <= 4:
+        print("Pen " ,end="")
+        b = b + 1
+
+    a = a  + 1
+    print()
+
+
+d = 7
+while d <= 10:
+    print("Python")
+    d = d + 1
+
+
+s = "raghavi"
+
+for i in s:
+    print(i)
+
+
+for i in range (11,20,1):
+    print(i)
+
+
+for i in range (2,50):
+    if i%5!=0:
+        print(i)
+
+
+for i in range (100,0,-1):
+    print(i)
+'''
+
+
+
+
+
