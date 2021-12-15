c63a100defbc01c9f49a62702541640df9562944
RaghaviP <raghav88pp@gmail.com>
2021-12-15 19:19:34 +0530

    worked on code

---
+++ My_Code_practice_2.py
@@ -0,0 +1,86 @@
+
+'''
+ch = input("enter a char")
+print(ch[10])
+
+
+g = int(input("enter 6th number"))
+h = int(input("enter 7th number"))
+i = g - h
+print(i)
+
+
+num = int(input("enter an integer:"))
+print(num)
+
+
+huy = input("enter your name")
+print(huy)
+
+
+ball = 20
+
+if ball >= 10:
+    print("it is red colour")
+
+print("thank you")
+
+
+
+c = 8
+r = c % 2
+
+if r == 0:
+    print("great")
+    if c < 10:
+        print("smaller")
+    else:
+        print("between")
+else:
+    print("not match")
+
+print("end")
+
+
+height = 167
+
+if height <= 175:
+    print("you are not eligible for modelling")
+    print("you cannot participate")
+elif height > 175 and height < 165:
+    print("you are not fit")
+else:
+    print("your are short")
+
+print("thank you")
+
+
+y = 3
+while y <= 8:
+    print("inovation")
+    z = 3
+    while z >= 7:
+        print("Idea")
+        z = z + 1
+
+    y = y + 1
+    print()
+
+z = 20
+
+while z <= 20:
+    print("euqal")
+    z = z + 1
+
+
+for i in range (20,50,1):
+    print(i)
+
+
+for i in range (6,30):
+    if i%3!=0:
+        print(i)
+'''
+
+
+
