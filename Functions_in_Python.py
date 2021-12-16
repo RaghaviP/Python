7564c28cbd45b5abe8962dc04cc082cc196bd7ba
RaghaviP <raghav88pp@gmail.com>
2021-12-16 20:19:31 +0530

    worked on functions

---
+++ Functions_in_Python.py
@@ -0,0 +1,66 @@
+
+#Function will execute task and it also return the scenario
+'''
+def greet():
+    print("Hello")
+    print("Good Morning")
+    print("How are you doing?")
+
+
+greet()
+
+
+def greet():
+    print("write Algorithm")
+
+greet()# this is  also called as calling
+
+
+def greet():# greet is function
+    print("Hello")#write statement(function name)
+    print("Good Morning")#write statement(function name)
+
+def add(x,y):# add is function ()this is known as argument
+    c = x + y
+    print(c)
+add(5,4)
+
+
+def greet():
+    print("Hello")
+    print("Good Morning")
+    print("How are you doing?")
+
+def add(x,y):
+    c = x + y
+    print(c)
+
+greet()
+add(5,4)
+
+
+def greet():
+    print("Hello")
+    print("Good Morning")
+    print("How are you doing?")
+
+def add(x,y):
+    c = x + y
+    return c #add the result and show me the value of c
+
+greet()
+result = add(5,4)
+print(result)
+
+
+def add_sub(x,y):
+    c = x + y
+    d = x - y
+    return c,d
+
+result1,result2 = add_sub(5,4)
+print(result1,result2)
+'''
+
+
+
