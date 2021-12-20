b501363134d069e4e14b421b159da9e8b82ab3c8
RaghaviP <raghav88pp@gmail.com>
2021-12-20 19:50:02 +0530

    Greetings from main function

---
+++ Python_main_function.py
@@ -0,0 +1,23 @@
+
+
+'''
+print("Good Moring")
+
+
+def main():
+    print("Hello python")
+
+
+
+print("Good Evening")
+'''
+
+print("Hi ShriRam")
+
+
+def main():
+    print(":)")
+
+
+print("How are you?")
+
