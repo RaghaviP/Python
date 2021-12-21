d848742fd0eafae7d4629797946591498be4d401
RaghaviP <raghav88pp@gmail.com>
2021-12-21 20:07:25 +0530

    Worked with While if in BCP

---
+++ BCP_End.py
@@ -0,0 +1,16 @@
+'''
+available = 20
+
+y = int(input("How many toffies you want?"))
+
+i = 1
+while i <= y:
+
+    if i > available:
+        break
+
+    print("Toffies")
+    i += 1
+
+print("End")
+'''
