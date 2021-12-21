95bf1ae1589f59c45688a2f01ae25d598ac7397f
RaghaviP <raghav88pp@gmail.com>
2021-12-21 19:52:40 +0530

    Worked on while loop BCP

---
+++ Loops_in _Break_Continuous_Pass.py
@@ -0,0 +1,17 @@
+
+'''
+av = 10
+
+a = int(input("How many Languages you want?"))
+
+i = 1
+while i <= a:
+
+    if i >av:
+        break
+
+    print("Python","GitHub")
+    i += 1
+
+print("End")
+'''
