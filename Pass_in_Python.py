c386e0658636636647368e26a11469b598603a26
RaghaviP <raghav88pp@gmail.com>
2021-12-21 20:32:10 +0530

    Worked on Pass in Python

---
+++ Pass_in_Python.py
@@ -0,0 +1,19 @@
+'''
+for i in range(1,101):
+
+    if(i%2!=0):
+        pass
+    else:
+        print(i)
+
+print("End")
+'''
+
+for i in range(1,200):
+
+    if(i%5!=0):
+        pass
+    else:
+        print(i)
+
+print("Code Ended")
