0257cea08a4ecd4fd73e328817ec2e79e52e2970
RaghaviP <raghav88pp@gmail.com>
2021-12-21 19:59:24 +0530

    Worked on basic BCP

---
+++ Basic_Break_Continue_Pass.py
@@ -0,0 +1,19 @@
+'''
+b = int(input("How many Graphics Card you want?"))
+
+i = 1
+while i <= b:
+
+    print("RTX3060")
+    i += 1
+
+
+x = int(input("How many Chocolates you want?"))
+
+i = 1
+while i <= x:
+
+    print("Dairymilk")
+    i += 1
+'''
+
