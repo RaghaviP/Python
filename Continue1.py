8473a17b6f0f5cc98ed8ff05ad8756bbfdf117f0
RaghaviP <raghav88pp@gmail.com>
2021-12-21 20:17:32 +0530

    Worked on Continue Python

---
+++ Continue1.py
@@ -0,0 +1,27 @@
+'''
+for i in range(1,101):
+    if i % 3 == 0:
+        continue
+
+    print(i)
+
+print("Bye")
+
+
+for i in range(0,100):
+    if i % 4 == 0:
+        continue
+
+    print(i)
+
+print("End")
+
+
+for i in range(0,50):
+    if i % 5 == 0:
+        continue
+
+    print(i)
+
+print("End")
+'''
