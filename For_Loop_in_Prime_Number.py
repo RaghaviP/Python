d873d7034f63a2867dcde6febb3000c640988ba1
RaghaviP <raghav88pp@gmail.com>
2021-12-24 13:40:58 +0530

    Worked on forloop in Prime number

---
+++ For_Loop_in_Prime_Number.py
@@ -0,0 +1,13 @@
+'''
+num = 56
+i = 1
+flag = 0;
+
+for i in range(2,num):
+
+    if (num % i) == 0:
+        print("Number is not prime,it is getting divided by:", i)
+        flag = 1
+if (flag == 0):
+        print("Number is prime")
+'''
