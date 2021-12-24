d37832ee51476bf5d2e726f395a10904fe89b589
RaghaviP <raghav88pp@gmail.com>
2021-12-24 14:25:15 +0530

    Worked in Practice24

---
+++ My_Python_Practice24.py
@@ -0,0 +1,13 @@
+'''
+print(2 + 6)
+print(2 - 6)
+print(2 * 6)
+print(2 / 6)
+print (5 // 2)
+print(5 + 2 - 10)
+print(2 * 5 + 3)
+print(5 + 5  * 6)
+print(10 % 3)
+print("Raghavi")
+print("Raghavi's Laptop")
+'''
