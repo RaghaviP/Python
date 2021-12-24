2448cadb4a008c3ead99aaf9500fb0a4106c8bc9
RaghaviP <raghav88pp@gmail.com>
2021-12-24 12:17:18 +0530

    Worked on For else1

---
+++ For_Else1.py
@@ -0,0 +1,11 @@
+'''
+for item in "Python":
+    if item == " h ":
+        break
+    print(item)
+
+else:
+    print("else block says for is successfully completed:")
+
+print("Job is Done!!")
+'''
