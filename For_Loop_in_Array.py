d85e589f20b5c3e3b24425eb6bc766b60aa4d66c
RaghaviP <raghav88pp@gmail.com>
2021-12-28 15:39:40 +0530

    Worked on for loop array

---
+++ For_Loop_in_Array.py
@@ -0,0 +1,15 @@
+'''
+from array import *
+
+vals = array('i',[2,5,8,-2,7,4])
+
+for i in range(6):
+     print(vals[i])
+'''
+
+from array import *
+
+vals = array('i',[2,5,-2,7,4])
+
+for e in vals:
+     print(e)
