a9d6ea8ea015452cf0fc31114a638d043b3d51bd
RaghaviP <raghav88pp@gmail.com>
2021-12-28 11:46:29 +0530

    Worked on C type signed int

---
+++ Array_in_Python.py
@@ -0,0 +1,12 @@
+from array import *
+# here I have used signed int c type 'i' in this we cannot use negative sign only positive then only the code run and no error found
+
+vals = array('i',[5,9,-8,4,2])
+
+
+print(vals)
+
+
+
+
+
