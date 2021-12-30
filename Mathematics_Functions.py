cdb8ffe7be692251b1d608fc0c6f14d4f155278f
RaghaviP <raghav88pp@gmail.com>
2021-12-30 22:25:26 +0530

    Worked on Maths functions

---
+++ Mathematics_Functions.py
@@ -0,0 +1,41 @@
+'''
+from numpy import *
+
+arr = array([5,8,6,3,7])
+
+print(sin(arr))
+
+
+from numpy import *
+
+trig = array([2,8,9,5,2])
+
+print(cos(trig))
+
+
+from numpy import *
+
+trig = array([5,8,9,4,5])
+
+print(log(trig))
+
+
+from numpy import *
+
+math = array([8,3,5,6,9])
+
+print(sqrt(math))
+
+
+from numpy import *
+
+arr = array([8,9,4,5,6])
+
+print(sum(arr))
+'''
+
+from numpy import *
+
+arr = array([2,3,4])
+
+print(max(arr))
