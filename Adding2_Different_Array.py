fc081bb3b35a2eb26a572b34c10bd9ad90f96c24
RaghaviP <raghav88pp@gmail.com>
2021-12-30 22:00:39 +0530

    Worked on Adding 2 array

---
+++ Adding2_Different_Array.py
@@ -0,0 +1,44 @@
+
+'''
+from numpy import *
+
+arr1 = array([6,7,8,9])
+arr2 = array([10,11,12,13])
+
+arr3 = arr1 + arr2
+
+print(arr3)
+
+
+from numpy import *
+
+arr4 = array([8,9,9])
+arr5 = array([8,5,6])
+
+arr6 = arr4 + arr5
+
+print(arr6)
+
+
+from numpy import *
+
+fruits = ["Cherry","Banana","Chiku","Watermelon"]
+print("Normal Sorting")
+fruits.sort()
+print(fruits)
+print("Reverse order sorting")
+fruits.sort(reverse=True)
+print(fruits)
+
+
+from numpy import *
+
+fruits = ["Apple","Peach","GoosBerry","Papaya"]
+
+Vegetables =["Carrot","Cucumber","Beetroot","BitterGaurd"]
+grocery = fruits + Vegetables
+
+print(grocery)
+'''
+
+
