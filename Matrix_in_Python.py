f54574c7c7e0372b282b595e8de68fa8d46271a6
RaghaviP <raghav88pp@gmail.com>
2022-01-03 19:53:59 +0530

    Worked on matrix

---
+++ Matrix_in_Python.py
@@ -0,0 +1,39 @@
+'''
+from numpy import *
+
+
+m = matrix('1 2 3; 6 4 5; 1 6 7')
+
+print(m)
+
+
+from numpy import *
+
+m1 = matrix('2 3 5; 8 7 9; 2 5 9')
+m2 = matrix('4 5 8; 2 4 5; 3 5 7')
+
+m3 = m1 + m2;
+
+print(m3)
+
+
+from numpy import *
+
+m1 = matrix('2 3 5; 8 7 9; 2 5 9')
+m2 = matrix('4 5 8; 2 4 5; 3 5 7')
+
+m3 = m1 - m2;
+
+print(m3)
+'''
+
+from numpy import *
+
+m1 = matrix('2 3 5; 8 7 9; 2 5 9')
+m2 = matrix('4 5 8; 2 4 5; 3 5 7')
+
+m3 = m1 * m2;
+
+print(m3)
+
+
