d179bf555c585f42d2ce0ed2020ca7bf7c06db0e
RaghaviP <raghav88pp@gmail.com>
2022-01-06 21:18:37 +0530

    worked on com type

---
+++ Class and objects.py
@@ -0,0 +1,17 @@
+# class is a type fun like computer class(keyword)
+# here we use variables and methods.
+
+class computer:
+    def config(self):
+        print("i5, 16gb, 1tb")
+
+#here com1 and com2 is object.
+com1 = computer()
+com2 = computer()
+
+com1.config()
+com2.config()
+
+
+
+
