41a63dacf05ad3bf6887726681472261bae13a51
RaghaviP <raghav88pp@gmail.com>
2022-01-12 18:56:44 +0530

    Worked on Inheritance

---
+++ Inheritance in OOPS.py
@@ -0,0 +1,22 @@
+class Employee:
+    def func_msg1(self):
+        print("Hello! ShriRam")
+
+    def fun_msg2(self):
+        print("Welocome to our Sphere")
+
+    def fun_msg3(self):
+        print("-*-*-*-*")
+
+class TL:
+    def fun_msg4(self):
+        print("OnBoard Apprication")
+
+employee1 = Employee()
+
+employee1.func_msg1()
+employee1.fun_msg2()
+employee1.fun_msg3()
+
+tl1 = TL()
+tl1.fun_msg4()
